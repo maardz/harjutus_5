d9843806fa93b0a6a871b979e69533629cb72494
maardz <moldermarge@gmail.com>
2014-09-30 02:14:16 +0300

    Add contant

--- index.php
+++ index.php
@@ -39,12 +39,81 @@
   	echo "Kas var 3 on NULL?"." ".is_null($var3);
   	echo "<br>";
   	echo "Kas var 4 on NULL?"." ".is_null($var4);
-  	echo "<br>"
+  	echo "<br>";
+  	echo "<h3>Set</h3>";
+  	echo "<br>";
+  	echo "Kas var 1 on väärtustatud?"." ".isset($var1);
+  	echo "<br>";
+  	echo "Kas var 2 on väärtustatud?"." ".isset($var2);
+  	echo "<br>";
+  	echo "Kas var 3 on väärtustatud?"." ".isset($var3);
+  	echo "<br>";
+  	echo "Kas var 4 on väärtustatud?"." ".isset($var4);
+  	echo "<br>";
+
+  	$var5 = array();
+  	$var6 = "0";
+  	$var7 = 0.0;
+  	$var8 = True;
+
+  	echo "Kas var 1 on empty?"." ".empty($var1);
+  	echo "<br>";
+  	echo "Kas var 2 on empty?"." ".empty($var2);
+  	echo "<br>";
+  	echo "Kas var 3 on empty?"." ".empty($var3);
+  	echo "<br>";
+  	echo "Kas var 4 on empty?"." ".empty($var4);
+  	echo "<br>";
+  	echo "Kas var 5 on empty?"." ".empty($var5);
+  	echo "<br>";
+  	echo "Kas var 6 on empty?"." ".empty($var6);
+  	echo "<br>";
+  	echo "Kas var 7 on empty?"." ".empty($var7);
+  	echo "<br>";
+  	echo "Kas var 8 on empty?"." ".empty($var8);
+  	echo "<br>";
 
   ?>
 
-  <h3>Set</h3>
-  
+  <h2>Andmetüübi teisendamine</h2>
+
+  <?php 
+  	 $count = 2;
+  	 echo $count;
+  	 echo "<br>";
+  	 echo gettype($count);
+  	 echo "<br>";
+  	 settype($count, "string");
+  	 echo settype($count, "string");
+  	 echo "<br>";
+  	 echo gettype($count);
+  	 echo "<br>";
+  	 echo settype($count, "float");
+  	 echo "<br>";
+  	 echo gettype($count);
+  	 echo "<br>";
+  	 settype($count, "array");
+  	 print_r($count, "array");
+  	 echo "<br>";
+  	 echo gettype($count);
+  	  echo "<br>";
+  	 echo settype($count, "boolean");
+  	 echo "<br>";
+  	 echo gettype($count);
+
+  ?>
+
+  <h2>Konstandid</h2>
+  <?php
+   define("MAX_WIDTH", 980); 
+   echo MAX_WIDTH;
+   echo "<br>";
+   define("MAX_WITDH", 500);
+   echo MAX_WIDTH;
+   echo "<br>";
+    
+
+  ?>
 
 </body>
 </html>
