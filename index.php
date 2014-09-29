081a882fe2bd641fc59f1988a989a0b97f6f87bb
maardz <moldermarge@gmail.com>
2014-09-30 01:05:39 +0300

    Add NULL

--- index.php
+++ index.php
@@ -25,6 +25,26 @@
   	echo "Kas result 3 on boolean?"." ".is_bool($result_3);
   	echo "<br>";
   ?>
+  <h2>Andmetüüpide olekud</h2>
+  <h3>NULL</h3>
+  <?php 
+  	$var1 = null;
+  	$var2 = "";
+  	$var3 = 0;
+  	$var4;
+  	echo "Kas var 1 on NULL?"." ".is_null($var1);
+  	echo "<br>";
+  	echo "Kas var 2 on NULL?"." ".is_null($var2);
+  	echo "<br>";
+  	echo "Kas var 3 on NULL?"." ".is_null($var3);
+  	echo "<br>";
+  	echo "Kas var 4 on NULL?"." ".is_null($var4);
+  	echo "<br>"
+
+  ?>
+
+  <h3>Set</h3>
+  
 
 </body>
 </html>
